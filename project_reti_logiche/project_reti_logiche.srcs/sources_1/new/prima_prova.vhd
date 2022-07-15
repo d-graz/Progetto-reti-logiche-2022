3302fcedb3da023c2bbe357c1ad3f077b3d71dc6
Davide Grazzani <davide.grazzani@mail.polimi.it>
2022-07-15 15:34:07 +0200

    creazione progetto VHDL

---
+++ project_reti_logiche/project_reti_logiche.srcs/sources_1/new/prima_prova.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 07/15/2022 03:32:01 PM
+-- Design Name: 
+-- Module Name: prima_prova - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity prima_prova is
+--  Port ( );
+end prima_prova;
+
+architecture Behavioral of prima_prova is
+
+begin
+
+
+end Behavioral;
