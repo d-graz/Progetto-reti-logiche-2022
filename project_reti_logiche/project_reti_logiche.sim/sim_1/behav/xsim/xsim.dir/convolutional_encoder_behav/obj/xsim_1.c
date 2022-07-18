50346e337844edebf82f30725a76cbd5718819ba
Davide Grazzani <davide.grazzani@mail.polimi.it>
2022-07-19 00:56:34 +0200

    minor changes

--- project_reti_logiche/project_reti_logiche.sim/sim_1/behav/xsim/xsim.dir/convolutional_encoder_behav/obj/xsim_1.c
+++ project_reti_logiche/project_reti_logiche.sim/sim_1/behav/xsim/xsim.dir/convolutional_encoder_behav/obj/xsim_1.c
@@ -54,19 +54,20 @@
 #endif
 typedef void (*funcp)(char *, char *);
 extern int main(int, char**);
-IKI_DLLESPEC extern void execute_14(char*, char *);
 IKI_DLLESPEC extern void execute_15(char*, char *);
+IKI_DLLESPEC extern void execute_16(char*, char *);
+IKI_DLLESPEC extern void execute_10(char*, char *);
 IKI_DLLESPEC extern void execute_11(char*, char *);
 IKI_DLLESPEC extern void transaction_1(char*, char*, unsigned, unsigned, unsigned);
 IKI_DLLESPEC extern void vhdl_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
-funcp funcTab[5] = {(funcp)execute_14, (funcp)execute_15, (funcp)execute_11, (funcp)transaction_1, (funcp)vhdl_transfunc_eventcallback};
-const int NumRelocateId= 5;
+funcp funcTab[6] = {(funcp)execute_15, (funcp)execute_16, (funcp)execute_10, (funcp)execute_11, (funcp)transaction_1, (funcp)vhdl_transfunc_eventcallback};
+const int NumRelocateId= 6;
 
 void relocate(char *dp)
 {
-	iki_relocate(dp, "xsim.dir/convolutional_encoder_behav/xsim.reloc",  (void **)funcTab, 5);
-	iki_vhdl_file_variable_register(dp + 3200);
-	iki_vhdl_file_variable_register(dp + 3256);
+	iki_relocate(dp, "xsim.dir/convolutional_encoder_behav/xsim.reloc",  (void **)funcTab, 6);
+	iki_vhdl_file_variable_register(dp + 3664);
+	iki_vhdl_file_variable_register(dp + 3720);
 
 
 	/*Populate the transaction function pointer field in the whole net structure */
--- project_reti_logiche/project_reti_logiche.srcs/sources_1/new/prima_prova.vhd
+++ project_reti_logiche/project_reti_logiche.srcs/sources_1/new/prima_prova.vhd
@@ -30,10 +30,13 @@ architecture behavioral of FF_D is
 end behavioral;
 
 architecture dataflow of FF_D is
-
+    signal output_inout : std_logic;
+    
     begin
-        output <= '0' when rst = '1' else
-                  input when rising_edge(clk);
+        output_inout <= '0' when rst = '1' else
+                  input when rising_edge(clk) else
+                  output_inout; 
+        output <= output_inout;
         -- debug : output non completamete specificato nel caso in cui non debba fare nulla
 end dataflow;
     
@@ -69,18 +72,100 @@ architecture structural of convolutional_encoder is
     begin
         FF1 : FF_D
             port map(input => u,clk => clk,rst => rst, output => FF1_signal);
-        
         FF2 : FF_D
             port map(input => FF1_signal,clk => clk,rst => rst, output => FF2_signal);
-        
         -- domanda : bisogna resettare il segnale in uscita dal covolutore -> per ora non implemento
-        -- p1k e p2k non sono completamenta specificate !
-        p1k <= u xor FF2_signal when falling_edge(clk);
-        p2k <= u xor FF1_signal xor FF2_signal when falling_edge(clk);
+        -- p1k e p2k non sono completamenta specificate ! -> vedere se funziona altrimenti specificare come ff_d
+        p1k <= u xor FF2_signal when clk = '1';
+        p2k <= u xor FF1_signal xor FF2_signal when clk = '1';
+
 end structural;
 
+-- string concatenator
 
--- convolutional wrapper used to create strings
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity string_managar is
+    port(
+    clk : in std_logic;
+    rst : in std_logic;
+    bit1 : in std_logic;
+    bit2 : in std_logic;
+    position : in std_logic_vector(2 downto 0);
+    half_z : out std_logic_vector(7 downto 0)
+    );
+end string_managar;
+
+architecture dataflow of string_managar is
+    signal index : unsigned;
+    
+    begin
+    index <= (2*unsigned(position)+1) when clk = '1' else index;
+    --assegnazione dei valori di output 
+    
+end dataflow;
+    
+
+
+-- controller to iterate between states
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity controller is
+    port(
+        clock : in std_logic;
+        reset : in std_logic;
+        start : in std_logic;
+        data : in std_logic_vector (7 downto 0);
+        convolutional_encoder_index : out std_logic_vector(2 downto 0);
+        done : out std_logic := '0';
+        mem_address : out std_logic_vector(15 downto 0);
+        mem_enable : out std_logic;
+        mem_write : out std_logic;
+        u : out std_logic
+    );
+end controller;
+
+architecture dataflow of controller is
+    type state_type is (read_wc,read,calculate,write);
+    constant bit : unsigned := "001";
+    signal next_state : state_type;
+    signal current_state : state_type := read_wc;
+    signal convolutional_encoder_inout : std_logic_vector(2 downto 0) := "111";
+    signal number_of_words : integer;
+    signal base_read : integer := 0;
+    signal base_write : integer := 1000;
+    
+    begin
+        done <= '1' when (number_of_words = 0 and current_state = read) else
+                '0' when reset = '1' else '0';
+        -- todo fixare il next state in caso di done per smettere di eseguire tutto, ricontrollare il ciclo degli stati
+        next_state <= read when current_state = write else
+                      read when (current_state = read_wc and start = '1') else
+                      calculate when current_state = read else
+                      write when (current_state = calculate and convolutional_encoder_inout = "000") else
+                      read_wc when reset = '1' else current_state;
+        current_state <= next_state when rising_edge(clock) else
+                         read_wc when reset = '1' else current_state;
+        convolutional_encoder_inout <= std_logic_vector(unsigned(convolutional_encoder_inout) - bit) when (current_state = calculate and falling_edge(clock)) else convolutional_encoder_inout;
+        convolutional_encoder_index <= convolutional_encoder_inout;
+        number_of_words <= number_of_words -1 when (current_state = read and falling_edge(clock)) else number_of_words;
+        base_read <= base_read+1 when ((current_state = read or current_state = read_wc) and falling_edge(clock)) else
+                     0 when reset = '1' else base_read;
+        base_write <= base_write+1 when (((current_state = calculate and convolutional_encoder_inout = "000") or current_state = write) and falling_edge(clock)) else
+                      1000 when reset = '1' else base_write;
+        mem_enable <= '1' when (current_state = read or (current_state = read_wc and start = '1') or current_state = write or ( current_state = calculate and convolutional_encoder_inout = "000")) else '0';
+        mem_write <= '1' when ((current_state = calculate and convolutional_encoder_inout = "000") or current_state = write) else '0';
+        -- todo : gestire data e base address
+        u <= data(to_integer(unsigned(convolutional_encoder_inout)));
+        mem_address <= std_logic_vector(to_unsigned(base_read,16)) when (current_state = read_wc or current_state = read) else
+                       std_logic_vector(to_unsigned(base_write,16)) when (current_state = write or (current_state = calculate and convolutional_encoder_inout = "111"));
+     
+end dataflow;
 
 
 -- project interface declaration
@@ -101,3 +186,36 @@ entity project_reti_logiche is
         o_data    : out std_logic_vector (7 downto 0)
     );
 end project_reti_logiche;
+
+architecture structural of project_reti_logiche is
+    
+    
+    component controller is
+        port(
+        clock : in std_logic;
+        reset : in std_logic;
+        start : in std_logic;
+        data : in std_logic_vector (7 downto 0);
+        convolutional_encoder_index : out std_logic_vector(2 downto 0);
+        done : out std_logic := '0';
+        mem_address : out std_logic_vector(15 downto 0);
+        mem_enable : out std_logic;
+        mem_write : out std_logic;
+        u : out std_logic
+    );
+    end component;
+    
+    component convolutional_encoder is
+        port(
+        u   : in std_logic;
+        clk : in std_logic;
+        rst : in std_logic;
+        p1k : out std_logic;
+        p2k : out std_logic
+    );
+    end component;
+    
+    begin
+    --logic : controller
+        -- port map(clock =>, reset =>, start =>, data =>, )
+end structural;
