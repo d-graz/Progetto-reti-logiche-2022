668f236b3aafe6f1f6d7d198705e514be65d42a0
Davide Grazzani <davide.grazzani@mail.polimi.it>
2022-07-20 19:33:04 +0200

    modifiche generali e corretto utilizzo dei segnani

--- project_reti_logiche/project_reti_logiche.sim/sim_1/behav/xsim/xsim.dir/project_tb_behav/obj/xsim_1.c
+++ project_reti_logiche/project_reti_logiche.sim/sim_1/behav/xsim/xsim.dir/project_tb_behav/obj/xsim_1.c
@@ -54,20 +54,41 @@
 #endif
 typedef void (*funcp)(char *, char *);
 extern int main(int, char**);
-IKI_DLLESPEC extern void execute_15(char*, char *);
-IKI_DLLESPEC extern void execute_16(char*, char *);
-IKI_DLLESPEC extern void execute_10(char*, char *);
-IKI_DLLESPEC extern void execute_11(char*, char *);
-IKI_DLLESPEC extern void transaction_1(char*, char*, unsigned, unsigned, unsigned);
+IKI_DLLESPEC extern void execute_63(char*, char *);
+IKI_DLLESPEC extern void execute_64(char*, char *);
+IKI_DLLESPEC extern void execute_65(char*, char *);
+IKI_DLLESPEC extern void execute_37(char*, char *);
+IKI_DLLESPEC extern void execute_38(char*, char *);
+IKI_DLLESPEC extern void execute_39(char*, char *);
+IKI_DLLESPEC extern void execute_40(char*, char *);
+IKI_DLLESPEC extern void execute_41(char*, char *);
+IKI_DLLESPEC extern void execute_42(char*, char *);
+IKI_DLLESPEC extern void execute_43(char*, char *);
+IKI_DLLESPEC extern void execute_44(char*, char *);
+IKI_DLLESPEC extern void execute_45(char*, char *);
+IKI_DLLESPEC extern void execute_46(char*, char *);
+IKI_DLLESPEC extern void execute_47(char*, char *);
+IKI_DLLESPEC extern void execute_48(char*, char *);
+IKI_DLLESPEC extern void execute_56(char*, char *);
+IKI_DLLESPEC extern void execute_57(char*, char *);
+IKI_DLLESPEC extern void execute_58(char*, char *);
+IKI_DLLESPEC extern void execute_51(char*, char *);
+IKI_DLLESPEC extern void execute_52(char*, char *);
+IKI_DLLESPEC extern void execute_60(char*, char *);
+IKI_DLLESPEC extern void execute_61(char*, char *);
+IKI_DLLESPEC extern void execute_62(char*, char *);
 IKI_DLLESPEC extern void vhdl_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
-funcp funcTab[6] = {(funcp)execute_15, (funcp)execute_16, (funcp)execute_10, (funcp)execute_11, (funcp)transaction_1, (funcp)vhdl_transfunc_eventcallback};
-const int NumRelocateId= 6;
+IKI_DLLESPEC extern void transaction_4(char*, char*, unsigned, unsigned, unsigned);
+IKI_DLLESPEC extern void transaction_11(char*, char*, unsigned, unsigned, unsigned);
+IKI_DLLESPEC extern void transaction_14(char*, char*, unsigned, unsigned, unsigned);
+funcp funcTab[27] = {(funcp)execute_63, (funcp)execute_64, (funcp)execute_65, (funcp)execute_37, (funcp)execute_38, (funcp)execute_39, (funcp)execute_40, (funcp)execute_41, (funcp)execute_42, (funcp)execute_43, (funcp)execute_44, (funcp)execute_45, (funcp)execute_46, (funcp)execute_47, (funcp)execute_48, (funcp)execute_56, (funcp)execute_57, (funcp)execute_58, (funcp)execute_51, (funcp)execute_52, (funcp)execute_60, (funcp)execute_61, (funcp)execute_62, (funcp)vhdl_transfunc_eventcallback, (funcp)transaction_4, (funcp)transaction_11, (funcp)transaction_14};
+const int NumRelocateId= 27;
 
 void relocate(char *dp)
 {
-	iki_relocate(dp, "xsim.dir/convolutional_encoder_behav/xsim.reloc",  (void **)funcTab, 6);
-	iki_vhdl_file_variable_register(dp + 3664);
-	iki_vhdl_file_variable_register(dp + 3720);
+	iki_relocate(dp, "xsim.dir/project_tb_behav/xsim.reloc",  (void **)funcTab, 27);
+	iki_vhdl_file_variable_register(dp + 1056664);
+	iki_vhdl_file_variable_register(dp + 1056720);
 
 
 	/*Populate the transaction function pointer field in the whole net structure */
@@ -75,12 +96,12 @@ void relocate(char *dp)
 
 void sensitize(char *dp)
 {
-	iki_sensitize(dp, "xsim.dir/convolutional_encoder_behav/xsim.reloc");
+	iki_sensitize(dp, "xsim.dir/project_tb_behav/xsim.reloc");
 }
 
 void simulate(char *dp)
 {
-		iki_schedule_processes_at_time_zero(dp, "xsim.dir/convolutional_encoder_behav/xsim.reloc");
+		iki_schedule_processes_at_time_zero(dp, "xsim.dir/project_tb_behav/xsim.reloc");
 	// Initialize Verilog nets in mixed simulation, for the cases when the value at time 0 should be propagated from the mixed language Vhdl net
 	iki_execute_processes();
 
@@ -102,9 +123,9 @@ extern SYSTEMCLIB_IMP_DLLSPEC char** xsim_argv_copy ;
 int main(int argc, char **argv)
 {
     iki_heap_initialize("ms", "isimmm", 0, 2147483648) ;
-    iki_set_sv_type_file_path_name("xsim.dir/convolutional_encoder_behav/xsim.svtype");
-    iki_set_crvs_dump_file_path_name("xsim.dir/convolutional_encoder_behav/xsim.crvsdump");
-    void* design_handle = iki_create_design("xsim.dir/convolutional_encoder_behav/xsim.mem", (void *)relocate, (void *)sensitize, (void *)simulate, (void*)0, 0, isimBridge_getWdbWriter(), 0, argc, argv);
+    iki_set_sv_type_file_path_name("xsim.dir/project_tb_behav/xsim.svtype");
+    iki_set_crvs_dump_file_path_name("xsim.dir/project_tb_behav/xsim.crvsdump");
+    void* design_handle = iki_create_design("xsim.dir/project_tb_behav/xsim.mem", (void *)relocate, (void *)sensitize, (void *)simulate, (void*)0, 0, isimBridge_getWdbWriter(), 0, argc, argv);
      iki_set_rc_trial_count(100);
     (void) design_handle;
     return iki_simulate_design();
--- project_reti_logiche/project_reti_logiche.srcs/sources_1/new/prima_prova.vhd
+++ project_reti_logiche/project_reti_logiche.srcs/sources_1/new/prima_prova.vhd
@@ -75,8 +75,8 @@ architecture structural of convolutional_encoder is
         FF2 : FF_D
             port map(input => FF1_signal,clk => controller_clk,rst => rst, output => FF2_signal);
         -- domanda : bisogna resettare il segnale in uscita dal covolutore -> per ora non implemento
-        p1k <= u xor FF2_signal when clk = '1' else p1k;
-        p2k <= u xor FF1_signal xor FF2_signal when clk = '1' else p2k;
+        p1k <= u xor FF2_signal when controller_clk = '1' else p1k;
+        p2k <= u xor FF1_signal xor FF2_signal when controller_clk = '1' else p2k;
         pk <= p1k & p2k;
 
 end structural;
@@ -87,21 +87,21 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity string_managar is
+entity string_manager is
     port(
     controller_clk : in std_logic;
     rst : in std_logic;
     bits : in std_logic_vector(1 downto 0);
     half_z : out std_logic_vector(7 downto 0)
     );
-end string_managar;
+end string_manager;
 
-architecture dataflow of string_managar is
+architecture dataflow of string_manager is
     signal half_z_inout : std_logic_vector(7 downto 0) := (others => '0');
-    signal counter : unsigned(2 downto 0) := "11";
-    constant bit1 : unsigned(2 downto 0) := "01";
-    constant zero : unsigned(2 downto 0) := "00";
-    constant full : unsigned(2 downto 0) := "11";
+    signal counter : unsigned(1 downto 0) := "11";
+    constant bit1 : unsigned(1 downto 0) := "01";
+    constant zero : unsigned(1 downto 0) := "00";
+    constant full : unsigned(1 downto 0) := "11";
 
     begin
         counter <= counter - bit1 when (falling_edge(controller_clk) and counter /= zero and rst = '0') else
@@ -138,55 +138,59 @@ entity controller is
 end controller;
 
 architecture dataflow of controller is
-    type state_type is (idle,r_wc,r,p_0,p_1,p_2,p_3,p_4,p_5,p_6,p_7);
+    type state_type is (idle,r_wc,r,p_0,p_1,p_2,p_3,p_4,p_5,p_6,p_7,d);
     signal next_state : state_type;
     signal current_state : state_type := idle;
-    signal number_of_words : integer;
+    signal number_of_words : integer := -1;
     signal base_read : integer := 0;
     signal base_write : integer := 1000;
-    signal done_inout : std_logic := '0';
+    signal mem_inout : std_logic_vector(7 downto 0);
     
     begin
-        done_inout <= '1' when (current_state = idle and number_of_words = 0 and reset = '0' and start = '1') else
-                      '0' when reset = '1' else 
-                      '0' when (current_state = r_wc and reset = '0') else done_inout;
-        done <= done_inout;
-        next_state <= idle when (start = '0' or current_state = p_7) else
-                      r_wc when (current_state = idle and done_inout = '-') else
-                      r when (current_state = r_wc or (current_state = idle and done_inout = '0')) else
-                      p_0 when (current_state = r) else
-                      p_1 when (current_state = p_0) else
-                      p_2 when (current_state = p_1) else
-                      p_3 when (current_state = p_2) else
-                      p_4 when (current_state = p_3) else
-                      p_5 when (current_state = p_4) else
-                      p_6 when (current_state = p_5) else
-                      p_7 when (current_state = p_6);
+        done <= '1' when (current_state = d and number_of_words = 0 and reset = '0' and start = '1') else
+                      '0' when (reset = '1' or start = '0');
+        mem_inout <= data when (current_state = p_0 and clock = '1') else mem_inout;
+        next_state <= idle when start = '0' else
+                      r_wc when (current_state = idle and start = '1') else
+                      r when (current_state = r_wc or (current_state = d and number_of_words /= 0)) else
+                      p_0 when current_state = r else
+                      p_1 when current_state = p_0 else
+                      p_2 when current_state = p_1 else
+                      p_3 when current_state = p_2 else
+                      p_4 when current_state = p_3 else
+                      p_5 when current_state = p_4 else
+                      p_6 when current_state = p_5 else
+                      p_7 when current_state = p_6 else
+                      d when current_state = p_7;
         current_state <= next_state when (rising_edge(clock) and reset = '0') else
                          idle when reset = '1' else current_state;
         with current_state select
-            u <= data(7) when p_0,
-                 data(6) when p_1,
-                 data(5) when p_2,
-                 data(4) when p_3,
-                 data(3) when p_4,
-                 data(2) when p_5,
-                 data(1) when p_6,
-                 data(0) when p_7,
-                 "-" when others;
-        base_read <= base_read + 1 when (falling_edge(clock) and (current_state = r_wc or current_state = r) and reset = '0') else
+            u <= mem_inout(7) when p_0,
+                 mem_inout(6) when p_1,
+                 mem_inout(5) when p_2,
+                 mem_inout(4) when p_3,
+                 mem_inout(3) when p_4,
+                 mem_inout(2) when p_5,
+                 mem_inout(1) when p_6,
+                 mem_inout(0) when p_7,
+                 '-' when others;
+        base_read <= base_read + 1 when (falling_edge(clock) and current_state = r and reset = '0') else
                      0 when reset = '1' else base_read;
-        base_write <= base_write +1 when (falling_edge(clock) and (current_state = p_3 or current_state = p_7) and reset = '0')else
+        base_write <= base_write +1 when (falling_edge(clock) and (current_state = p_4 or current_state = d) and reset = '0')else
                       1000 when reset = '1' else base_write;
-        number_of_words <= to_integer(unsigned(data)) when current_state = r_wc else
-                           number_of_words - 1 when (current_state = r and falling_edge(clk)) else number_of_words;
-        mem_address <= std_logic_vector(to_unsigned(base_read,16)) when current_state = r else 
-                       std_logic_vector(to_unsigned(base_write,16)) when (current_state = p_3 or current_state = p_7) else mem_address;
-        mem_enable <= '1' when (current_state = r_wc or current_state = r or current_state = p_3 or current_state = p_7) else '0';
+                      -- todo fix lettura numero di parole dopo che avviene la r_wc
+        number_of_words <= to_integer(unsigned(data)) when (current_state = r_wc) else
+                           number_of_words - 1 when (current_state = r and falling_edge(clock)) else
+                           -1 when reset = '1' else number_of_words;
+        -- warning : mem_address non completamente specificata
+        mem_address <= std_logic_vector(to_unsigned(base_read,16)) when (current_state = r_wc or current_state = r) else 
+                       std_logic_vector(to_unsigned(base_write,16)) when (current_state = p_3 or current_state = p_7);
+        -- waringn mem enable non completamente specificata
+        mem_enable <= '1' when ((current_state = idle and next_state = r_wc) or current_state = r_wc or current_state = r or current_state = p_3 or current_state = p_7) else '0';
         mem_write <= '1' when (current_state = p_3 or current_state = p_7) else '0';
-        controller_clk <= '1' when (rising_edge(clock) and current_state /= idle and current_state /= r_wc and current_state /= r) else
-                          '0' when falling_edge(clock) else controller_clk;
-     
+        -- warning : controller_clk non completamente specificata
+        controller_clk <= '1' when (current_state'event and current_state /= idle and current_state /= r_wc and current_state /= r and current_state /= d) else
+                          '0' when falling_edge(clock);
 end dataflow;
 
 
@@ -238,7 +242,7 @@ architecture structural of project_reti_logiche is
         );
     end component;
 
-    component string_managar is
+    component string_manager is
         port(
         controller_clk : in std_logic;
         rst : in std_logic;
@@ -247,13 +251,15 @@ architecture structural of project_reti_logiche is
         );
     end component;
     
-    controller : controller
-        port map(clock =>i_clk,reset=>i_rst,start =>i_start,data =>i_data, done =>o_done, mem_address =>o_address, mem_enable =>o_en, mem_write =>o_we, u =>u, controller_clk =>controller_clk);
+    begin
+    
+        cont : controller
+            port map(clock =>i_clk,reset=>i_rst,start =>i_start,data =>i_data, done =>o_done, mem_address =>o_address, mem_enable =>o_en, mem_write =>o_we, u =>u, controller_clk =>controller_clk);
 
-    encoder : convolutional_encoder
-        port map(u =>u,controller_clk =>controller_clk, rst =>i_rst, pk =>conv_encoder_out);
+        encoder : convolutional_encoder
+            port map(u =>u,controller_clk =>controller_clk, rst =>i_rst, pk =>conv_encoder_out);
 
-    string_manager : string_manager
-        port map(controller_clk =>controller_clk, rst =>i_rst, bits =>conv_encoder_out, half_z =>o_data);
+        str_mng: string_manager
+            port map(controller_clk =>controller_clk, rst =>i_rst, bits =>conv_encoder_out, half_z =>o_data);
 
 end structural;
